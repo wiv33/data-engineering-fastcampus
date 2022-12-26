cec2191d8bec02bb0e00958fdfd064fe55d47694
PSK <wiv821253@gmail.com>
2022-12-26 10:07:42 +0900

    update 5-1 - Spark Streaming

---
+++ src/main/scala/_02/streaming.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.appName("stream-word-count").getOrCreate()
+
+lines_df = spark.readStream.format("socket").option("host", "localhost").option("port", "9999").load()
+
+words_df = lines_df.select(expr("explode(split(value, ' ')) as word"))
+counts_df = words_df.groupBy("word").count()
+
+word_count_query = counts_df.writeStream.format("console") \
+    .outputMode("complete") \
+    .option("checkpointLocation", ".checkpoint") \
+    .start()
+word_count_query.awaitTermination()
+
+# nc -lk 9999
+# > pyspark spark sql streaming
+# > spark graphx pyspark
+# > counter batch filter
